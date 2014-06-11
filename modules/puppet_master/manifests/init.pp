390e649db4b9235a287075f96dd1cb2b5312497e
David Kowis <david.kowis@rackspace.com>
2014-06-11 18:10:42 -0500

    Adding the puppetmaster's puppet configs

--- modules/puppet_master/manifests/init.pp
+++ modules/puppet_master/manifests/init.pp
@@ -1,6 +1,63 @@
 class puppet_master {
-    # using https://github.com/TomPoulton/hiera-eyaml
-    package{"hiera-eyaml":
 
+    # using https://github.com/TomPoulton/hiera-eyaml
+    # need our eyaml backend
+    package{"hiera-eyaml":
+        ensure   => installed,
+        provider => 'gem',
+    }
+
+    package{"mod_passenger":
+        ensure => present,
+    }
+
+    package{"httpd":
+        ensure => present,
+    }
+
+    file{"/srv/puppetmaster":
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => "0755",
+    }
+
+    file{"/srv/puppetmaster/public":
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => "0755",
+        require => File['/srv/puppetmaster']
+    }
+
+    file{"/srv/puppetmaster/config.ru":
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => "0644",
+        require => File['/srv/puppetmaster'],
+        notify => Service['httpd'],
+    }
+
+    # this is using the keys provided by puppetmaster when it installs,
+    # a rebuild from zero would require resigning all the keys from the various puppet agents.
+    # possibly something we'd want to puppetize? heh
+    file{"/etc/httpd/conf.d/puppetmaster.conf":
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 0664,
+        content => template("puppet_master/puppetmaster-vhost.conf.erb"),
+        require => Package['httpd', 'mod_passenger'],
+        notify => Service['httpd'],
+    }
+
+    service{"httpd":
+        ensure => running,
+        enable => true,
+        require => [
+            Package['httpd', 'mod_passenger'],
+            File['/etc/httpd/conf.d/puppetmaster.conf', '/srv/puppetmaster/public', '/srv/puppetmaster/config.ru']
+        ]
     }
 }
