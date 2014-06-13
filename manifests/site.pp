ddce58f3c8ac86cda242e0731b7cd606f0abab8e
David Kowis <david.kowis@rackspace.com>
2014-06-13 10:49:29 -0500

    Adding firewall stuff.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,16 @@
 
+# we want all firewall resources purged, so only the puppet ones apply
+resources { "firewall":
+    purge => true
+}
+
+Firewall {
+    before  => Class['base::fw_post'],
+    require => Class['base::fw_pre'],
+}
+
+class {'firewall': }
+
 node default {
     include users
     include base
