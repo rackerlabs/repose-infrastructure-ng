04435ffc2e9ccf7a2fd647accd63ed964e7c4ed0
David Kowis <dkowis@shlrm.org>
2014-06-15 15:49:40 -0500

    Updating the bootstrap for a puppet client!

--- bootstrap-puppet-client.sh
+++ bootstrap-puppet-client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-#TODO: OUT OF DATE, modify this to use puppet agent
+# Applies to debian wheezy, not to centos any longer.
 
-wget -O /tmp/epel-release-5-4.noarch.rpm http://download.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-sudo rpm -Uvh /tmp/epel-release-5-4.noarch.rpm
-rm -f /tmp/epel-release-5-4.noarch.rpm
+REPODEB="puppetlabs-release-wheezy.deb"
 
-sudo yum install -y puppet
-sudo yum install -y git
+wget -O /tmp/$REPODEB https://apt.puppetlabs.com/$REPODEB &&
+dpkg -i /tmp/$REPODEB &&
+rm -f /tmp/$REPODEB &&
 
-cd /etc/puppet
-git clone https://github.com/rackerlabs/repose-infrastructure-management.git modules
+apt-get update &&
+apt-get install -y puppet
+
+echo "First run of puppet agent, need to sign the cert on the puppet master!"
+echo "It'll wait 2 minutes for the cert to be signed, so go sign it!"
+puppet agent --test --waitforcert 120
