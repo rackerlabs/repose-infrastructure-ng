dd7c9b6d6edbb11c53205572e5c04fbbbcbdd344
David Kowis <david.kowis@rackspace.com>
2014-10-29 13:18:21 -0500

    Set the ssl cert, bind address, and the user password appropriately

--- myModules/repose_sonar/manifests/database.pp
+++ myModules/repose_sonar/manifests/database.pp
@@ -1,17 +1,13 @@
 class repose_sonar::database(
-    $mysql_password = undef,
     $mysql_root_password = undef
 ) {
+
+    include ssl_cert
+
     firewall{'110 mysql access':
         port => [3306],
         proto => tcp,
         action => accept,
-    }
-
-    # add a postgresql database for the sonars
-    # don't manage the firewall, because it's only going to connect on localhost
-    if($mysql_password == undef) {
-        fail("Postgres password must be specified!")
     }
 
     user{'mysql':
@@ -26,6 +22,16 @@ class repose_sonar::database(
     # have to set up the mysql database now
     class {'::mysql::server':
         root_password => $mysql_root_password,
+        override_options => {
+            "mysqld" => {
+                "bind-address" => "0.0.0.0",
+                "ssl" => "true",
+                "ssl-capath" => "/etc/ssl/certs",
+                "ssl-cert" => "/etc/ssl/certs/openrepose.crt",
+                "ssl-key" => "/etc/ssl/keys/openrepose.key"
+            }
+        },
+        require => Class['ssl_cert'],
     }
 
     mysql_database{'sonar@%':
@@ -36,6 +42,7 @@ class repose_sonar::database(
 
     mysql_user{'sonar@%/sonar.*':
         ensure => 'present',
+        password_hash => mysql_password(hiera('repose_sonar::sonar_jdbc')['password']),
     }
 
     mysql_grant{'sonar@%/sonar.*':
@@ -43,5 +50,4 @@ class repose_sonar::database(
         table => 'sonar.*',
         user => 'sonar@%',
     }
-
 }
