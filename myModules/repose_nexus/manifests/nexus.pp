79f1d2134acd0eefebfbe1d3f5df64046bb48ec0
David Kowis <david.kowis@rackspace.com>
2014-10-30 14:03:29 -0500

    More work on the nexus bits

--- myModules/repose_nexus/manifests/nexus.pp
+++ myModules/repose_nexus/manifests/nexus.pp
@@ -18,9 +18,50 @@ class repose_nexus::nexus(
     }
 
     exec{ 'nexus-untar':
-        command => "tar xf ${archive}",
-        cwd     => '/opt',
-        creates => "/opt/nexus-${version}",
-        path    => ['/bin', '/usr/bin']
+        command     => "tar xf ${archive}",
+        cwd         => '/opt',
+        creates     => "/opt/nexus-${version}",
+        path        => ['/bin', '/usr/bin'],
+        notify      => Exec['nexus-dir-perms'],
+        refreshonly => true,
+    }
+
+    file{ 'nexus-symlink':
+        path   => '/opt/nexus',
+        ensure => link,
+        target => "/opt/nexus-${version}",
+    }
+
+    exec{ 'nexus-dir-perms':
+        command     => "chown nexus:nexus logs tmp",
+        cwd         => "/opt/nexus-${version}",
+        path        => ['/bin', '/usr/bin'],
+        require     => User['nexus'],
+        refreshonly => true,
+    }
+
+    file{ '/etc/init.d/nexus':
+        ensure => file,
+        source => "puppet:///modules/repose_nexus/nexus",
+        owner  => root,
+        group  => root,
+        mode   => 0744,
+    }
+
+    user{ 'nexus':
+        ensure => present,
+        uid    => 5000,
+        gid    => 5000,
+        home   => '/srv/sonatype-work',
+        shell  => '/bin/bash',
+    }
+
+    service{ 'nexus':
+        ensure  => running,
+        enable  => true,
+        require => [
+            User['nexus'],
+            File['nexus-symlink', '/etc/init.d/nexus']
+        ],
     }
 }
