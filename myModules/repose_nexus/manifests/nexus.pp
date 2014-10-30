0cf0406316f0674f12086ef5c833e8be7a394ef3
David Kowis <david.kowis@rackspace.com>
2014-10-30 14:05:06 -0500

    Derp

--- myModules/repose_nexus/manifests/nexus.pp
+++ myModules/repose_nexus/manifests/nexus.pp
@@ -48,12 +48,18 @@ class repose_nexus::nexus(
         mode   => 0744,
     }
 
+    group{'nexus':
+        ensure => present,
+        gid => 5000,
+    }
+
     user{ 'nexus':
         ensure => present,
         uid    => 5000,
         gid    => 5000,
         home   => '/srv/sonatype-work',
         shell  => '/bin/bash',
+        require => Group['nexus'],
     }
 
     service{ 'nexus':
