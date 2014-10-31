366c6cafb6e3e156df8e9816864d531f17d727ea
David Kowis <dkowis@shlrm.org>
2014-10-31 17:04:34 -0500

    Adding a base::nginx profile so I can use it in other classes

---
+++ myModules/base/manifests/nginx.pp
@@ -0,0 +1,58 @@
+class base::nginx {
+
+    include ssl_cert
+
+    package { 'nginx':
+        ensure => present,
+    }
+
+    exec{ 'dhparam-gen':
+        command => "openssl dhparam -out /etc/nginx/dhparam.pem 2048",
+        path    => ['/bin', '/usr/bin'],
+        cwd     => "/etc/nginx",
+        creates => "/etc/nginx/dhparam.pem",
+    }
+
+# Don't want the default site running.
+    file{ "/etc/nginx/sites-enabled/default":
+        ensure  => absent,
+        require => Package['nginx'],
+    }
+
+    firewall{ '100 nginx http/s access':
+        port   => [443,80],
+        proto  => tcp,
+        action => accept,
+    }
+
+    file{ '/etc/nginx/conf.d/00-https-redirect.conf':
+        ensure  => file,
+        mode    => 0644,
+        owner   => root,
+        group   => root,
+        content => template('base/nginx-https-redirect.conf.erb'),
+        require => Package['nginx'],
+        notify  => Service['nginx'],
+    }
+
+    file{ '/etc/nginx/nginx-ssl.conf':
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 0644,
+        source  => "puppet:///modules/base/nginx-ssl-config.conf",
+        require => Class['ssl_cert'],
+    }
+
+    service{ 'nginx':
+        ensure    => running,
+        enable    => true,
+        subscribe => Class['ssl_cert'],
+        require   => [
+            Package['nginx'],
+            Class['ssl_cert'],
+            Exec['dhparam-gen'],
+            File['/etc/nginx/sites-enabled/default']
+        ]
+    }
+}
