0db9d68c68f0a0c131c5fa0e4f1251343f860bbf
David Kowis <david.kowis@rackspace.com>
2014-11-03 09:27:59 -0600

    Moved the autohttps stuff into it's own class
    THis way I can possibly not include the autohttps stuff if I want to

--- myModules/base/manifests/nginx.pp
+++ myModules/base/manifests/nginx.pp
@@ -25,16 +25,6 @@ class base::nginx {
         action => accept,
     }
 
-    file{ '/etc/nginx/conf.d/00-https-redirect.conf':
-        ensure  => file,
-        mode    => 0644,
-        owner   => root,
-        group   => root,
-        content => template('base/nginx-https-redirect.conf.erb'),
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
-
     file{ '/etc/nginx/nginx-ssl.conf':
         ensure  => file,
         owner   => root,
---
+++ myModules/base/manifests/nginx/autohttps.pp
@@ -0,0 +1,14 @@
+class base::nginx::autohttps{
+    include base::nginx
+
+    file{ '/etc/nginx/conf.d/00-https-redirect.conf':
+        ensure  => file,
+        mode    => 0644,
+        owner   => root,
+        group   => root,
+        content => template('base/nginx-https-redirect.conf.erb'),
+        require => Package['nginx'],
+        notify  => Service['nginx'],
+    }
+
+}
