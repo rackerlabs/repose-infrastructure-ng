9d5ffcea1dc414c029eb8de1e0d86b335cb9dba0
David Kowis <david.kowis@rackspace.com>
2015-01-27 15:33:11 -0600

    Setting up the nrpe client stuff

--- myModules/repose_nagios/manifests/client.pp
+++ myModules/repose_nagios/manifests/client.pp
@@ -3,15 +3,24 @@ class repose_nagios::client {
     include repose_nagios
 # this class is for things that aren't going to run nagios directly, but will probably run nrpe
     package { 'nagios-nrpe-server':
-
+        ensure => present,
     }
 
+# add a firewall rule
+    firewall{ '100 accept TCP 5666':
+        port   => 5666,
+        proto  => tcp,
+        action => accept,
+    }
+
+# Bah, nrpe doesn't use /etc/nagios3
     file{ 'nrpe-config':
-        path   => '/etc/nagios/nrpe_local.conf',
-        owner  => root,
-        group  => root,
-        mode   => 0644,
-        source => 'puppet:///modules/repose_nagios/client_nrpe.conf',
+        path    => '/etc/nagios/nrpe_local.conf',
+        owner   => root,
+        group   => root,
+        mode    => 0644,
+        source  => 'puppet:///modules/repose_nagios/client_nrpe.conf',
+        require => Package['nagios-nrpe-server']
     }
 
     service{ 'nagios-nrpe-server':
