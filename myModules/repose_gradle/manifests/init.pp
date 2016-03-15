cf7e9569561123b794660c5b048bfc4f39d29ebf
Adrian George <adrian.george@rackspace.com>
2016-03-15 12:30:57 -0500

    Updated to latest gradle

--- myModules/repose_gradle/manifests/init.pp
+++ myModules/repose_gradle/manifests/init.pp
@@ -1,6 +1,6 @@
 #Installs gradle
 class repose_gradle(
-  $version = '2.11',
+  $version = '2.12',
   $base_url = 'http://downloads.gradle.org/distributions',
   $user = undef,
   $user_home = "/home/${user}",
