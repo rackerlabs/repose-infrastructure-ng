957234c41666a7381541e3981f3347ab3296a334
wdschei <Bill.Scheidegger@Rackspace.com>
2016-05-11 13:47:59 -0500

    Updated the Client Bootstrap script to clean up all the loose ends.

--- bootstrap-puppet-client.sh
+++ bootstrap-puppet-client.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Applies to debian wheezy, not to centos any longer.
+# Applies to debian flavors only now.
 # Since the boxes should have a proper hostname of something.openrepose.org
 # they will know how to find the puppet master already (puppet.openrepose.org)
 
-REPODEB="puppetlabs-release-jessie.deb"
+REPODEB="puppetlabs-release-$(lsb_release --short --codename).deb"
 
 wget -O /tmp/$REPODEB https://apt.puppetlabs.com/$REPODEB &&
 dpkg -i /tmp/$REPODEB &&
@@ -15,4 +15,5 @@ apt-get install -y puppet
 
 echo "First run of puppet agent, need to sign the cert on the puppet master!"
 echo "It's now executing puppet agent --test as the end of the script!"
+puppet agent --enable
 puppet agent --test
