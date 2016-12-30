9675fecc63e5d6ef8b1972db3d2ff6701197b81c
Damien Johnson <dmn.johns@gmail.com>
2016-12-29 20:47:43 -0600

    Using Rackspace DNS servers for Docker

--- myModules/repose_jenkins/manifests/init.pp
+++ myModules/repose_jenkins/manifests/init.pp
@@ -21,7 +21,7 @@ class repose_jenkins(
 
     # ensure docker is installed to verify releases, and use Google's DNS server to prevent resolution issues
     class{ 'docker':
-        dns => '8.8.8.8',
+        dns => ['72.3.128.241', '72.3.128.240'],
     }
 
     $jenkins_home = '/var/lib/jenkins'
