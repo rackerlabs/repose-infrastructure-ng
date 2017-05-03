006e18cb0d470944a8b6047a21acf472a9c288fd
Adrian George <adrian.george@rackspace.com>
2017-05-03 12:39:18 -0500

    Now with backups?

--- myModules/repose_grafana/manifests/init.pp
+++ myModules/repose_grafana/manifests/init.pp
@@ -29,4 +29,51 @@ class repose_grafana {
     jump    => 'REDIRECT',
     toports => '3000'
   }
+
+  $grafana_backups = '/srv/grafana-backups'
+  backup_cloud_files::target { 'performance_grafana':
+    target            => $grafana_backups,
+    cf_username       => hiera('rs_cloud_username'),
+    cf_apikey         => hiera('rs_cloud_apikey'),
+    cf_region         => 'DFW',
+    duplicity_options => '--full-if-older-than 15D --volsize 250 --exclude-other-filesystems --no-encryption',
+  }
+
+  cron { 'grafana_backup':
+    ensure  => present,
+    command => "cp -rf /var/lib/grafana $grafana_backups",
+    user    => 'root',
+    hour    => 12,
+    minute  => 0,
+    require => Class['grafana'],
+  }
+
+  cron { 'duplicity_backup':
+    ensure  => present,
+    command => '/usr/local/bin/duplicity_performance_grafana.rb',
+    user    => root,
+    hour    => 13,
+    minute  => 0,
+    require => Backup_cloud_files::Target['performance_grafana'],
+  }
+
+  # schedule a clean up of the backups once a month
+  cron { 'duplicity_cleanup':
+    ensure   => present,
+    command  => '/usr/local/bin/duplicity_performance_grafana.rb remove-older-than 1M --force \$url',
+    user     => root,
+    monthday => 1,
+    hour     => 3,
+    minute   => 0,
+    require  => Backup_cloud_files::Target['performance_grafana'],
+  }
+
+  #Papertrail the grafana logs
+  $papertrail_port = hiera("base::papertrail_port", 1)
+  class { 'remotesyslog':
+    port => $papertrail_port,
+    logs => [
+      '/var/log/grafana/grafana.log'
+    ],
+  }
 }
