7ef0e8c1bf408c3e620386d2a54ee4c1e4653ef3
Adrian George <adrian.george@rackspace.com>
2017-05-12 09:19:53 -0500

    Moved all the slave specific things to the slave manifest

--- myModules/repose_jenkins/manifests/init.pp
+++ myModules/repose_jenkins/manifests/init.pp
@@ -1,137 +1,10 @@
 # Installs base layer, but no jenkins itself. Perfect for a slave configuration.
 # also used by the master, basically anything that wants to run our jenkins jobs
 # Depends on garethr/remotesyslog to activate remote syslog for things on this host
-class repose_jenkins(
-    $maven_version = "3.2.1",
-    $deploy_key = undef,
-    $deploy_key_pub = undef,
-    $repo_key = undef,
-    $repo_key_pub = undef,
-    $inova_username = undef,
-    $inova_password = undef,
-    $research_nexus_username = undef,
-    $research_nexus_password = undef,
-    $saxon_ee_license = undef
-) {
-
-    # class to ensure jenkins is installed
-    # this will default to gradle 2.8, have to use a different syntax to get it with other versions
-    # or set the values in hiera
-    include repose_jenkins::gpgkey
-
-    # ensure docker is installed to verify releases, and give the docker0
-    # bridge a static IP to match firewall rules
-    class { 'docker':
-        bip => '172.17.0.1/16',
-    }
-
-    # Docker requires certain iptables rules to be setup for containers to
-    # access the internet. Docker will automatically setup these rules when the
-    # service is run. However, the firewall Puppet module will purge the rules
-    # that Docker set up whenever it runs. Setting the iptables rules through
-    # the firewall module should solve this issue.
-    firewallchain { 'DOCKER:nat:IPv4':
-        ensure => 'present',
-        purge  => 'true',
-    }->
-    firewall { '200 route local through DOCKER':
-        table    => 'nat',
-        chain    => 'PREROUTING',
-        proto    => 'all',
-        dst_type => 'LOCAL',
-        jump     => 'DOCKER',
-    }->
-    firewall { '201 OUTPUT LOCAL through DOCKER':
-        table       => 'nat',
-        chain       => 'OUTPUT',
-        proto       => 'all',
-        destination => '! 127.0.0.0/8',
-        dst_type    => 'LOCAL',
-        jump        => 'DOCKER',
-    }->
-    firewall { '202 MASQUERADE output interface not docker0':
-        table    => 'nat',
-        chain    => 'POSTROUTING',
-        proto    => 'all',
-        source   => '172.17.0.0/16',
-        outiface => '! docker0',
-        jump     => 'MASQUERADE',
-    }->
-    firewall { '203 RETURN input interface docker0':
-        table   => 'nat',
-        chain   => 'DOCKER',
-        proto   => 'all',
-        iniface => 'docker0',
-        jump    => 'RETURN',
-    }
-
-    firewallchain { 'DOCKER:filter:IPv4':
-        ensure => 'present',
-        purge  => 'true',
-    }->
-    firewallchain { 'DOCKER-ISOLATION:filter:IPv4':
-        ensure => 'present',
-        purge  => 'true',
-    }->
-    firewall { '300 FORWARD to DOCKER-ISOLATION':
-        table => 'filter',
-        chain => 'FORWARD',
-        proto => 'all',
-        jump  => 'DOCKER-ISOLATION',
-    }->
-    firewall { '301 route to docker0 through DOCKER':
-        table    => 'filter',
-        chain    => 'FORWARD',
-        proto    => 'all',
-        outiface => 'docker0',
-        jump     => 'DOCKER',
-    }->
-    firewall { '302 ACCEPT states to docker0 FORWARD':
-        table    => 'filter',
-        chain    => 'FORWARD',
-        proto    => 'all',
-        outiface => 'docker0',
-        ctstate  => ['RELATED', 'ESTABLISHED'],
-        action   => 'accept',
-    }->
-    firewall { '303 ACCEPT docker0 to not docker0 FORWARD':
-        table    => 'filter',
-        chain    => 'FORWARD',
-        proto    => 'all',
-        iniface  => 'docker0',
-        outiface => '! docker0',
-        action   => 'accept',
-    }->
-    firewall { '304 ACCEPT docker0 to docker0 FORWARD':
-        table    => 'filter',
-        chain    => 'FORWARD',
-        proto    => 'all',
-        iniface  => 'docker0',
-        outiface => 'docker0',
-        action   => 'accept',
-    }->
-    firewall { '305 RETURN DOCKER-ISOLATION':
-        table => 'filter',
-        chain => 'DOCKER-ISOLATION',
-        proto => 'all',
-        jump  => 'RETURN',
-    }
+class repose_jenkins {
 
     $jenkins_home = '/var/lib/jenkins'
     $github_key_info = hiera_hash("base::github_host_key", { "key" => "DEFAULT", "type" => "ssh-rsa" })
-
-    class{"repose_gradle":
-        user      => 'jenkins',
-        user_home => "${jenkins_home}",
-        daemon    => false,
-        require  => User["jenkins"],
-    }
-
-    class{"repose_maven":
-        user      => 'jenkins',
-        user_home => "${jenkins_home}",
-        require  => User["jenkins"],
-    }
 
 # this should ensure we've got java on the system at jdk8. Installed via package
 # it will give us JDK8 however.
@@ -147,28 +20,7 @@ class repose_jenkins(
         version      => 'present',
     }
 
-# anything that's going to run jenkins stuff will need rpm
-    package { 'rpm':
-    # I don't think we care about the version here...
-        ensure => present,
-    }
-
-    package { 'expect':
-    # This is needed by the rpm-maven-plugin
-        ensure => present,
-    }
-
-    package { 'dpkg-sig':
-    # This is needed by the jdeb maven plugin
-        ensure => present,
-    }
-
     package { 'git':
-        ensure => present,
-    }
-
-    package { 'gcc':
-        # This is needed by API-Checker's use of Nailgun
         ensure => present,
     }
 
@@ -180,24 +32,6 @@ class repose_jenkins(
         owner  => jenkins,
         group  => jenkins,
         source => "puppet:///modules/repose_jenkins/jenkins-gitconfig",
-    }
-
-    group { 'jenkins':
-        ensure => present,
-    }
-
-    group { 'docker':
-        ensure => present,
-    }
-
-    user { 'jenkins':
-        ensure     => present,
-        gid        => 'jenkins',
-        groups     => 'docker',
-        home       => $jenkins_home,
-        shell      => '/bin/bash',
-        managehome => true,
-        require    => Group['docker'],
     }
 
     Sshkey<|title == 'github.com'|> {
@@ -215,76 +49,12 @@ class repose_jenkins(
         user    => 'jenkins'
     }
 
-# this is the old jenkins key from the old server, it can go away eventually.
-    ssh_authorized_key { 'old_jenkins':
-        ensure  => present,
-        type    => 'ssh-rsa',
-        key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAuy/VESlo9iZAa9YQbEv9JGvvEsRKC3HxW2XivlDchGOxUNfrdaBGtFjMPe5rf6Qlv1hJ8bvHqZgCQIWYigRF45GXPJXGCaMWFoADG5+Mtr4SfoOWE8i6rVRphaKdIDV+UlhNQWlr4Cw/K4sgJB671qbSQjkn1H2uHiECMB1iUBtE8aOyDQm2bNzHh2sVyrDbUDm7zU354dIo84r3HhHVsK+3d0IhkiIhtWXc7IH4wL0pJ8B2Iv6FVLsQlY+pibGBPQzns25j83bPN01tj2JAxe6EqgsUyIJVu3Hb4UpFWkWquLQnOg0xbRHP/UnK5bQb/NI1ly/HKvt9xxQH8cEERQ==',
-        user    => 'jenkins'
-    }
-
-    file { "${jenkins_home}/.ssh":
-        ensure  => directory,
-        owner   => jenkins,
-        group   => jenkins,
-        mode    => '0700',
-    }
-
-    file{ "${jenkins_home}/.ssh/id_rsa":
-        ensure  => file,
-        mode    => 0600,
-        owner   => jenkins,
-        group   => jenkins,
-        content => "${deploy_key}",
-        require => File["${jenkins_home}/.ssh"],
-    }
-
-    file{ "${jenkins_home}/.ssh/id_rsa.pub":
-        mode    => 0600,
-        owner   => jenkins,
-        group   => jenkins,
-        content => "${deploy_key_pub}",
-        require => File["${jenkins_home}/.ssh"],
-    }
-
-    file{ "${jenkins_home}/.ssh/repo_key":
-        mode    => 0600,
-        owner   => jenkins,
-        group   => jenkins,
-        content => "${repo_key}",
-        require => File["${jenkins_home}/.ssh"],
-    }
-
-    file{ "${jenkins_home}/.ssh/repo_key.pub":
-        mode    => 0600,
-        owner   => jenkins,
-        group   => jenkins,
-        content => "${repo_key_pub}",
-        require => File["${jenkins_home}/.ssh"],
-    }
-
+    #not certain what this is doing? I think this needs to move to the master manifest?
     file { "${jenkins_home}/plugins":
         ensure  => directory,
         owner   => jenkins,
         group   => jenkins,
         mode    => '0755',
         require => User['jenkins']
-    }
-
-    file{ "${jenkins_home}/saxon_ee":
-        ensure  => directory,
-        owner   => jenkins,
-        group   => jenkins,
-        mode    => 0750,
-        require => User['jenkins'],
-    }
-
-    file { "${jenkins_home}/saxon_ee/saxon-license.lic":
-        ensure  => file,
-        owner   => jenkins,
-        group   => jenkins,
-        mode    => 0440,
-        content => "${saxon_ee_license}",
-        require => File["${jenkins_home}/saxon_ee"],
     }
 }
