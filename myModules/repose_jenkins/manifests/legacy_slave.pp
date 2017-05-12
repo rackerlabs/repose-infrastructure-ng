7996cdfe4ddfe268dbbad9af4ab5dac74c913640
Adrian George <adrian.george@rackspace.com>
2017-05-12 10:16:45 -0500

    Broke up the details of the slave manifests

--- myModules/repose_jenkins/manifests/legacy_slave.pp
+++ myModules/repose_jenkins/manifests/legacy_slave.pp
@@ -1,3 +1,29 @@
 class repose_jenkins::legacy_slave {
-  include repose_jenkins::slave
+
+  class { 'repose_jenkins::slave':
+    java_package => 'openjdk-7-jdk'
+  }
+
+  class{"repose_maven":
+    user      => 'jenkins',
+    user_home => "${repose_jenkins::jenkins_home}",
+    require   => User["jenkins"],
+  }
+
+  # anything that's going to run jenkins stuff will need rpm
+  package { 'rpm':
+    # I don't think we care about the version here...
+    ensure => present,
+  }
+
+  package { 'expect':
+    # This is needed by the rpm-maven-plugin
+    ensure => present,
+  }
+
+  package { 'dpkg-sig':
+    # This is needed by the jdeb maven plugin
+    ensure => present,
+  }
+
 }
