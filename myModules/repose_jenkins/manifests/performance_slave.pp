53b47cef1f47409366645bf76a321d8838bdf771
Adrian George <adrian.george@rackspace.com>
2017-05-16 14:33:01 -0500

    added the pip package installation

--- myModules/repose_jenkins/manifests/performance_slave.pp
+++ myModules/repose_jenkins/manifests/performance_slave.pp
@@ -4,6 +4,23 @@ class repose_jenkins::performance_slave {
 
   package { ['build-essential', 'software-properties-common', 'python-software-properties', 'python-pip',
              'python2.7-dev', 'python-setuptools', 'sshpass', 'libffi-dev', 'libssl-dev']:
-    ensure  => present,
+    ensure => present,
   }
-}
+
+  python::pip { 'markupsafe' :
+    pkgname => 'markupsafe',
+  }
+
+  python::pip { 'ansible' :
+    pkgname => 'ansible',
+    ensure  => '2.3.0.0',
+  }
+
+  python::pip { 'pyrax' :
+    pkgname => 'pyrax',
+  }
+
+  python::pip { 'six' :
+    pkgname      => 'six',
+    install_args => '--upgrade',
+  }}
