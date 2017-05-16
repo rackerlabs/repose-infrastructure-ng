ad27a15dc9653000fad8f2159d5dac8fd15fe5b3
Adrian George <adrian.george@rackspace.com>
2017-05-16 14:13:05 -0500

    Added all the needed apt packages

--- myModules/repose_jenkins/manifests/performance_slave.pp
+++ myModules/repose_jenkins/manifests/performance_slave.pp
@@ -1,4 +1,9 @@
 class repose_jenkins::performance_slave {
 
   include 'repose_jenkins::modern_slave'
+
+  package { ['build-essential', 'software-properties-common', 'python-software-properties', 'python-pip',
+             'python2.7-dev', 'python-setuptools', 'sshpass', 'libffi-dev', 'libssl-dev']:
+    ensure  => present,
+  }
 }
