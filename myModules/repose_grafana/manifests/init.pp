ad7b2794f4ed511d70e28be02fa7637f217a3b18
wdschei <Bill.Scheidegger@Rackspace.com>
2018-03-28 10:25:41 -0500

    Upgraded Grafana from v5.0.3 to v5.0.4.

--- myModules/repose_grafana/manifests/init.pp
+++ myModules/repose_grafana/manifests/init.pp
@@ -5,7 +5,7 @@ class repose_grafana {
 
   class { 'grafana':
     install_method => 'repo',
-    version        => '5.0.3',
+    version        => '5.0.4',
     cfg            => {
       app_mode => 'production',
       users    => {
