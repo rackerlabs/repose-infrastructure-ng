96bcc15dc3439cff695be5943c2cfa29456377dc
Damien Johnson <damien.johnson@rackspace.com>
2018-09-10 09:05:26 -0500

    Upgraded Grafana from v5.2.3 to v5.2.4

--- myModules/repose_grafana/manifests/init.pp
+++ myModules/repose_grafana/manifests/init.pp
@@ -5,7 +5,7 @@ class repose_grafana {
 
   class { 'grafana':
     install_method => 'repo',
-    version        => '5.2.3',
+    version        => '5.2.4',
     cfg            => {
       app_mode         => 'production',
       users            => {
