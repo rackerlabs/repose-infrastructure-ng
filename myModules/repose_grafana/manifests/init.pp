283c4b10c96bf03b8229ebff8e6baa71daa443b9
Damien Johnson <dmn.johns@gmail.com>
2018-12-10 13:41:37 -0600

    Upgraded Grafana from v5.4.0 to v5.4.1

--- myModules/repose_grafana/manifests/init.pp
+++ myModules/repose_grafana/manifests/init.pp
@@ -5,7 +5,7 @@ class repose_grafana {
 
   class { 'grafana':
     install_method => 'repo',
-    version        => '5.4.0',
+    version        => '5.4.1',
     cfg            => {
       app_mode         => 'production',
       users            => {
