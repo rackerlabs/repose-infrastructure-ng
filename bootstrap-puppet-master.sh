d8a079f752b3ba9053ff9e530ece4651c48c8791
wdschei <Bill.Scheidegger@Rackspace.com>
2019-02-11 10:14:52 -0600

    Updated to the new filesystem locations.

--- bootstrap-puppet-master.sh
+++ bootstrap-puppet-master.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # set it up to work with debians now
-wget https://apt.puppetlabs.com/puppet6-release-$(lsb_release -cs).deb &&
-dpkg -i puppet6-release-$(lsb_release -cs).deb &&
+MAJ_VER="6"
+CODENAME=$(lsb_release -cs)
+wget https://apt.puppetlabs.com/puppet${MAJ_VER}-release-${CODENAME}.deb &&
+dpkg -i puppet${MAJ_VER}-release-${CODENAME}.deb &&
 apt update &&
 
 # install needed things
@@ -23,17 +25,15 @@ git clone https://github.com/rackerlabs/repose-infrastructure-ng.git puppet &&
 echo "Installing librarian-puppet and bundler" &&
 gem install bundler librarian-puppet hiera-eyaml --no-rdoc --no-ri &&
 
-cd /etc/puppet &&
-mkdir -p ssl &&
-
-#ensure links are clean
-rm -rf /etc/puppet/modules     &&
-rm -rf /etc/puppet/hiera.yaml  &&
-rm -rf /etc/puppet/hieradata   &&
-rm -rf /etc/puppet/manifests   &&
-rm -rf /etc/puppet/puppet.conf &&
+# ensure links are clean
+rm -rf /etc/puppetlabs/puppet/modules     &&
+rm -rf /etc/puppetlabs/puppet/hiera.yaml  &&
+rm -rf /etc/puppetlabs/puppet/hieradata   &&
+rm -rf /etc/puppetlabs/puppet/manifests   &&
+rm -rf /etc/puppetlabs/puppet/puppet.conf &&
 
 # create symlinks to the git repo
+cd /etc/puppetlabs/puppet     &&
 ln -s /srv/puppet/modules     &&
 ln -s /srv/puppet/hiera.yaml  &&
 ln -s /srv/puppet/hieradata   &&
@@ -43,15 +43,12 @@ ln -s /srv/puppet/puppet.conf &&
 # actually install the modules onto the server
 # if you ever want to use different modules, you have to update the modules, and run the install again
 cd /srv/puppet &&
-librarian-puppet install --no-use-v1-api &&
+librarian-puppet install --no-use-v1-api
 
-# I think also it uses /etc/hiera.yaml
-ln -sf /srv/puppet/hiera.yaml /etc/hiera.yaml &&
-
-echo "PREP DONE! Last steps:"
+echo -e "\n\nPREP DONE! Last steps:"
 echo "Put the eyaml backend cert and key onto this system"
-echo "at  /etc/puppet/ssl/private_key.pkcs7.pem"
-echo "and /etc/puppet/ssl/public_key.pkcs7.pem"
+echo "at  /etc/puppetlabs/puppet/ssl/private_key.pkcs7.pem"
+echo "and /etc/puppetlabs/puppet/ssl/public_key.pkcs7.pem"
 echo "run puppet master --no-daemonize --verbose one time to generate the SSL certs it needs"
 echo "kill it after a few moments, so it's generated the ssl certs"
-echo "then run puppet apply /etc/puppet/manifests/puppet_master.pp"
+echo "then run puppet apply /etc/puppetlabs/puppet/manifests/puppet_master.pp"
