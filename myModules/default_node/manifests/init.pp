63ce64cf70a6f93efd34aaca5340bed87619afe4
wdschei <Bill.Scheidegger@Rackspace.com>
2019-02-25 13:17:57 -0600

    Moving back from the Resource-like behavior to the Include-like.

--- myModules/default_node/manifests/init.pp
+++ myModules/default_node/manifests/init.pp
@@ -1,13 +1,15 @@
 class default_node (
   $puppet_version = "6.2.0-1${lsbdistcodename}"
 ) {
-    # Hold the Puppet Agent package at the desired version so that Nagios does not
-    # alert us of updates that we don't want to consume.
-    apt::pin { 'puppet_agent':
-        packages => 'puppet-agent',
-        version  => $puppet_version,
-        priority => 1001,
-    }
+  include base
+
+  # Hold the Puppet Agent package at the desired version so that Nagios does not
+  # alert us of updates that we don't want to consume.
+  apt::pin { 'puppet_agent':
+      packages => 'puppet-agent',
+      version  => $puppet_version,
+      priority => 1001,
+  }
 
   class { 'users': }
   # we want all firewall resources purged, so only the puppet ones apply
@@ -24,9 +26,7 @@ class default_node (
   }
 
   class { ['base::fw_pre', 'base::fw_post']: }
-  class { 'base': }
   class { 'cloud_monitoring': }
-
   class { 'firewall': }
 
   # just setting swap size to a 4GB swapfile no matter what
