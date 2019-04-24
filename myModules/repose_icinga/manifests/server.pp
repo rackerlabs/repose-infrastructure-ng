cb8a7cc10b8d32e00abcd65567be4a255f7fcb58
wdschei <Bill.Scheidegger@Rackspace.com>
2019-04-24 17:47:54 -0500

    Minor updates and formatting.

--- myModules/repose_icinga/manifests/server.pp
+++ myModules/repose_icinga/manifests/server.pp
@@ -1,20 +1,29 @@
-class repose_icinga::server(
-    $icinga2_user = undef,
-    $icinga2_pass = undef,
-    $icingaweb2_user = undef,
-    $icingaweb2_pass = undef,
+class repose_icinga::server (
+    $icinga2_db        = undef,
+    $icinga2_user      = undef,
+    $icinga2_pass      = undef,
+    $icingaweb2_db     = undef,
+    $icingaweb2_user   = undef,
+    $icingaweb2_pass   = undef,
     $icinga2admin_user = undef,
     $icinga2admin_pass = undef,
+    $icinga2admin_hash = undef,
 ) {
     include repose_icinga
-    include ::icinga2
     include ::postgresql::server
 
     class { '::icinga2':
         manage_repo => true,
-        confd     => false,
-        features  => ['checker','mainlog','notification','statusdata','compatlog','command'],
-        constants => {
+        confd       => false,
+        features    => [
+            'checker',
+            'mainlog',
+            'notification',
+            'statusdata',
+            'compatlog',
+            'command',
+        ],
+        constants   => {
             'ZoneName' => 'master',
         },
     }
