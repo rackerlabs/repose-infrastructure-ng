22c7d248a689885a03aef9f418f0c879b1cb566e
Damien Johnson <damien.johnson@rackspace.com>
2019-08-12 12:07:04 -0500

    Upgraded Grafana from v6.2.5 to v6.3.2

--- myModules/repose_grafana/manifests/init.pp
+++ myModules/repose_grafana/manifests/init.pp
@@ -1,7 +1,7 @@
 class repose_grafana {
   class { 'grafana':
     install_method => 'repo',
-    version        => '6.2.5',
+    version        => '6.3.2',
     cfg            => {
       app_mode         => 'production',
       users            => {
